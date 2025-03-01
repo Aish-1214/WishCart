8eef06fc8f323157686acfa0d0cd2b462f3a080d
Aish-1214 <aishwaryahegde1214@gmail.com>
2025-03-01 10:23:08 +0530

    this is first commit

---
+++ src/Pages/Error.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Error() {
+  return (<div>
+ Error 404 : Page Not Found 
+ <img src="https://www.scopycode.com/includes/images/blog/error_404.gif" alt=""/>
+  </div>
+  );
+}
