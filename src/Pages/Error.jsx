955328ca4f3fe2b34e036209cd4699b99f5a47a0
Aish-1214 <aishwaryahegde1214@gmail.com>
2025-03-01 12:05:16 +0530

    'i-did-changes-in-alt-text'

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Error() {
   return (<div>
  Error 404 : Page Not Found 
- <img src="https://www.scopycode.com/includes/images/blog/error_404.gif" alt=""/>
+ <img src="https://www.scopycode.com/includes/images/blog/error_404.gif" alt="hello git new branch 01-03-2025-a"/>
   </div>
   );
 }
